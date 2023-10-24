b7be4ea6b5312a543d05c70bf0b7e4d94c7cd85a
plphuc <phamluumyphuc@gmail.com>
2023-10-24 19:43:38 +0700

    [add] ecrypt feature

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,74 +1,28 @@
-import Embed from '@editorjs/embed';
-import Table from '@editorjs/table';
-import List from '@editorjs/list';
-import Code from '@editorjs/code';
-import LinkTool from '@editorjs/link';
-import Image from '@editorjs/image';
-import Header from '@editorjs/header';
-import Quote from '@editorjs/quote';
-import Marker from '@editorjs/marker';
-import CheckList from '@editorjs/checklist';
-import Delimiter from '@editorjs/delimiter';
-import InlineCode from '@editorjs/inline-code';
-import SimpleImage from '@editorjs/simple-image';
+export const digestPassword = async (passString) => {
+  // String to Array Buffer
+  const passBuff = new TextEncoder().encode(passString);
 
-export const EDITOR_JS_TOOLS = {
-  embed: Embed,
-  table: Table,
-  marker: Marker,
-  list: List,
-  code: Code,
-  linkTool: LinkTool,
-  image: {
-    class: Image,
-    config: {
-      uploader: {
-        async uploadByFile(file) {
-          const formData = new FormData();
-          formData.append('file', file);
-
-          const response = await fetch(`http://localhost:8080/uploadImage/uploadByFile`, {
-            method: 'POST',
-            mode: 'cors',
-            body: formData,
-          });
-
-          const data = await response.json();
-
-          if (data.success === 1) {
-            return data;
-          }
-        },
-        async uploadByUrl(url) {
-          const response = await fetch(`http://localhost:8080/uploadImage/uploadByUrl`, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({url: url}),
-          });
-
-          const data = await response.json();
-
-          if (data.success === 1) {
-            return data;
-          }
-        },
-      },
+  const hashedPassword = await window.crypto.subtle.digest(
+    {
+        name: "SHA-256",
     },
-  },
-  header: {
-    class: Header,
-    config: {
-      placeholder: 'Enter a header',
-      levels: [1, 2, 3, 4],
-      defaultLevel: 3,
-    },
-  },
-  quote: Quote,
-  checklist: CheckList,
-  delimiter: Delimiter,
-  inlineCode: InlineCode,
-  simpleImage: SimpleImage,
-};
+    new Uint8Array(passBuff) //The data you want to hash as an ArrayBuffer
+)
+.catch(function(err){
+    console.error(err);
+});
+
+return hashedPassword
+}
+
+export const buff_to_base64 = (buff) => btoa(
+  new Uint8Array(buff).reduce(
+    (data, byte) => data + String.fromCharCode(byte), ''
+  )
+); 
+
+export const isEqualBuff = (buff1, buff2) => {
+  const str1 = buff_to_base64(buff1)
+  const str2 = buff_to_base64(buff2)
+  return str1 === str2
+}
