411ddda5a5c6578e0d0ba72783c04cf6e3641be9
plphuc <phamluumyphuc@gmail.com>
2023-10-25 18:41:10 +0700

    [add] components

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import styles from './NotFoundPage.module.css'
+
+function NotFoundPage(props) {
+  return (
+    <div className={styles.wrapper}>
+      hello
+    </div>
+  );
+}
+
+export default NotFoundPage;
