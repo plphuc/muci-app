69e524f0a18bc1147456f557c47c089096b65c87
plmphuc <plm.phuc@tego.global>
2024-07-25 10:06:07 +0700

    change routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import WelcomePage from 'pages/WelcomePage/WelcomePage'
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage'
-import TopLayout from 'layouts/TopLayout/TopLayout'
-import LoginPage from 'pages/LoginPage/LoginPage'
-import RegisterPage from 'pages/RegisterPage/RegisterPage'
-import HomePage from 'pages/HomePage/HomePage'
-import TimerFeature from 'pages/HomePage/components/SidebarSection/TimerFeature/TimerFeature'
-import MainSection from 'pages/HomePage/components/MainSection/MainSection'
+import { RouterProvider } from 'react-router-dom'
+import routers from 'routes'
 
 // import styles from 'App.module.css';
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes path="/">
-                    <Route element={<TopLayout />}>
-                        <Route index element={<WelcomePage />} />
-                        <Route path="login" element={<LoginPage />} />
-                        <Route path="register" element={<RegisterPage />} />
-                    </Route>
-                    <Route path="/:username" element={<HomePage />}>
-                        <Route path="" element={<MainSection />}></Route>
-                        <Route
-                            path="/:username/timer"
-                            element={<TimerFeature />}
-                        ></Route>
-                    </Route>
-
-
-                    <Route path="/*" element={<NotFoundPage />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
+    // return (
+    //     <>
+    //         <BrowserRouter>
+    //             <Routes path="/" element={<TopLayout />}>
+    //                 <Route index element={<WelcomePage />} />
+    //                 <Route path="login" element={<LoginPage />} />
+    //                 <Route path="register" element={<RegisterPage />} />
+    //                 <Route path="/:username" element={<HomePage />}>
+    //                     <Route index element={<MainSection />}/>
+    //                     <Route
+    //                         path="/:username/timer"
+    //                         element={<TimerFeature />}
+    //                     ></Route>
+    //                 </Route>
+    //                 <Route path="/*" element={<NotFoundPage />} />
+    //             </Routes>
+    //         </BrowserRouter>
+    //     </>
+    // )
+    return <RouterProvider router={routers}></RouterProvider>
 }
 
 export default App
